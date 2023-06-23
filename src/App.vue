52d09b096ec4e09059db1a869455d8c601076bbe
Max Kuba <kuba.m.s.ua@gmail.com>
2023-06-23 04:16:07 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #1E1E1E;
+}
+</style>
